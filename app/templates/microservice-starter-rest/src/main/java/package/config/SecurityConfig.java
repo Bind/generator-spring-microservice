f42c19dbfd43ffc7ff15c83806a4206ab12fc80c
Andreas Bjørnådal <bjornadal@gmail.com>
2016-09-12 14:27:55 +0200

    Using WebSecurityConfigurerAdapter

--- app/templates/microservice-starter-rest/src/main/java/package/config/SecurityConfig.java
+++ app/templates/microservice-starter-rest/src/main/java/package/config/SecurityConfig.java
@@ -4,18 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-public class SecurityConfig extends ResourceServerConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.requestMatchers()
-                .antMatchers("/**")
-                .and()
-                .authorizeRequests()
-                .anyRequest().authenticated();
+        http
+            .authorizeRequests()
+                .anyRequest().permitAll();
     }
 }
