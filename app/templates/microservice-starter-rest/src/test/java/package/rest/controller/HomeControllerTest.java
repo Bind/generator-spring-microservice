d91f50916ae144f5f728bd87be56e902d488bb59
Andreas Bjørnådal <bjornadal@gmail.com>
2017-01-17 08:39:16 +0100

    Removed deprecated import

--- app/templates/microservice-starter-rest/src/test/java/package/rest/controller/HomeControllerTest.java
+++ app/templates/microservice-starter-rest/src/test/java/package/rest/controller/HomeControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
