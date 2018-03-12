eafed1339fd1df1d4145e03e2218ab87f487e428
Jim Arthur Nilsen <jim.nilsen@nb.no>
2018-03-12 09:55:33 +0100

    Updated

--- app/index.js
+++ app/index.js
@@ -1,181 +1,296 @@
 'use strict';
-var util = require('util');
-var path = require('path');
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
+// Require dependencies
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
 
-var SpringGenerator = module.exports = function SpringGenerator(args, options, config) {
-    yeoman.Base.apply(this, arguments);
-};
+module.exports = class extends Generator {
 
-util.inherits(SpringGenerator, yeoman.Base);
+    initializing() {
+        this.pkg = require('../package.json');
+    }
 
-SpringGenerator.prototype.askFor = function askFor() {
-    var cb = this.async();
+    prompting() {
+        console.log(chalk.blue(
+            '\n.............DD88888888888888888,............\n' +
+            '...........:888888888888888888888,...........\n' +
+            '..........+88888888888888888888888+..........\n' +
+            '.........,8888888888888888888888888..........\n' +
+            '.........888888888888...888888888888.........\n' +
+            '.......,88888887..D88...88Z..88888888,.......\n' +
+            '.......8888888,...888...88D...=8888888.......\n' +
+            '......D888888,..$8888...88887...8888888......\n' +
+            '.....Z888888$..I88888...88888:..88888888,....\n' +
+            '....D8888888...888888...88888D..,88888888....\n' +
+            '....88888888,..888888..,888888...88888888....\n' +
+            '....88888888,..8888888$888888D..,88888888....\n' +
+            '....88888888I..Z8888888888888+..888888888....\n' +
+            '.....Z8888888...O888888888888..,88888888.....\n' +
+            '......88888888...,88888888D...,88888888......\n' +
+            '.......88888888=.....?I+.....I88888888.......\n' +
+            '.......,88888888D7.........ZD88888888,.......\n' +
+            '.........888888888888888888888888888.........\n' +
+            '.........,8888888888888888888888888..........\n' +
+            '..........+88888888888888888888888+..........\n' +
+            '...........,888888888888888888888:...........\n' +
+            '.............DD888888888888888DD.............\n' +
+            chalk.blue('' +
+                '\nWelcome to the Spring Boot Microservice Generator\n\n'
+            )));
+        const prompts = [
+            {
+                type: 'string',
+                name: 'packageName',
+                message: '(1/5) What is your default package name?',
+                default: 'com.example.myservice'
+            },
+            {
+                type: 'string',
+                name: 'baseName',
+                message: '(2/5) What is the base name of service?',
+                default: 'myservice'
+            },
+            {
+                type: 'string',
+                name: 'serviceDescription',
+                message: '(3/5) Give a short description of service.',
+                default: 'This Microservice does awesome things'
+            },
+            {
+                type: 'string',
+                name: 'dockerRegistry',
+                message: '(4/5) What is your Docker registry?',
+                default: ''
+            },
+            {
+                type: 'string',
+                name: 'dockerPrefix',
+                message: '(5/5) What is your Docker prefix?',
+                default: 'example'
+            }
+        ];
 
-    console.log(chalk.blue('\n.............DD88888888888888888,............\n' +
-        '...........:888888888888888888888,...........\n' +
-        '..........+88888888888888888888888+..........\n' +
-        '.........,8888888888888888888888888..........\n' +
-        '.........888888888888...888888888888.........\n' +
-        '.......,88888887..D88...88Z..88888888,.......\n' +
-        '.......8888888,...888...88D...=8888888.......\n' +
-        '......D888888,..$8888...88887...8888888......\n' +
-        '.....Z888888$..I88888...88888:..88888888,....\n' +
-        '....D8888888...888888...88888D..,88888888....\n' +
-        '....88888888,..888888..,888888...88888888....\n' +
-        '....88888888,..8888888$888888D..,88888888....\n' +
-        '....88888888I..Z8888888888888+..888888888....\n' +
-        '.....Z8888888...O888888888888..,88888888.....\n' +
-        '......88888888...,88888888D...,88888888......\n' +
-        '.......88888888=.....?I+.....I88888888.......\n' +
-        '.......,88888888D7.........ZD88888888,.......\n' +
-        '.........888888888888888888888888888.........\n' +
-        '.........,8888888888888888888888888..........\n' +
-        '..........+88888888888888888888888+..........\n' +
-        '...........,888888888888888888888:...........\n' +
-        '.............DD888888888888888DD.............\n' +
-        chalk.blue('\nWelcome to the Spring Boot Microservice Generator\n\n')));
+        return this.prompt(prompts).then(answers => {
+            this.packageName = answers.packageName;
+            this.baseName = answers.baseName;
+            this.starters = answers.starters;
+            this.dockerPrefix = answers.dockerPrefix;
+            this.dockerRegistry = answers.dockerRegistry;
+            this.serviceDescription = answers.serviceDescription;
+        });
+    }
 
-    var prompts = [
-        {
-            type: 'string',
-            name: 'packageName',
-            message: '(1/5) What is your default package name?',
-            default: 'com.example.myservice'
-        },
-        {
-            type: 'string',
-            name: 'baseName',
-            message: '(2/5) What is the base name of service?',
-            default: 'myservice'
-        },
-        {
-            type: 'string',
-            name: 'serviceDescription',
-            message: '(3/5) Give a short description of service.',
-            default: 'This Microservice does awesome things'
-        },
-        {
-            type: 'string',
-            name: 'dockerRegistry',
-            message: '(4/5) What is your Docker registry?',
-            default: ''
-        },
-        {
-            type: 'string',
-            name: 'dockerPrefix',
-            message: '(5/5) What is your Docker prefix?',
-            default: 'example'
-        }
-    ];
-
-    this.prompt(prompts, function (props) {
-        this.packageName = props.packageName;
-        this.baseName = props.baseName;
-        this.starters = props.starters;
-        this.dockerPrefix = props.dockerPrefix;
-        this.dockerRegistry = props.dockerRegistry;
-        this.serviceDescription = props.serviceDescription;
-
-        cb();
-    }.bind(this));
-};
-
-SpringGenerator.prototype.app = function app() {
-    // ----------------------------
-    // Micro service starter REST
-    // ----------------------------
-    var packageFolder = this.packageName.replace(/\./g, '/');
-    var restDir = this.baseName + '-rest/';
-    var restDirTemplate = 'microservice-starter-rest/';
-    var javaDir = restDir + 'src/main/java/' + packageFolder + '/';
-    var javaDirTemplate = restDirTemplate + 'src/main/java/package/';
-    var dockerDir = restDir + 'src/main/docker/';
-    var dockerDirTemplate = restDirTemplate + 'src/main/docker/';
-    var resourceDir = restDir + 'src/main/resources/';
-    var resourceDirTemplate = restDirTemplate + 'src/main/resources/';
-    var testDir = restDir + 'src/test/java/' + packageFolder + '/';
-    var testDirTemplate = restDirTemplate + 'src/test/java/package/';
-
-    // Docker
-    //this.template(dockerDirTemplate + 'Dockerfile', dockerDir  + 'Dockerfile', this, {});
-
-    // Resource
-    this.template(resourceDirTemplate + 'application.yml', resourceDir  + 'application.yml', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
-    //this.template(resourceDirTemplate + 'application-dev.yml', resourceDir  + 'application-dev.yml', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
-    //this.template(resourceDirTemplate + 'application-prod.yml', resourceDir  + 'application-prod.yml', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
-    this.template(resourceDirTemplate + 'bootstrap.yml', resourceDir  + 'bootstrap.yml', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
-
-    // Test
-    //this.template(testDirTemplate + 'ApplicationTests.java', testDir + 'ApplicationTests.java', this, {});
-    this.template(testDirTemplate + 'rest/controller/HomeControllerTest.java', testDir + 'rest/controller/HomeControllerTest.java', this, {});
-    this.template(testDirTemplate + 'core/package-info.java', testDir + 'core/package-info.java', this, {});
+    writing() {
+        this.packageFolder = this.packageName.replace(/\./g, '/');
+        this._generateRest();
+        this._generateModel();
+        this._generateIT();
+        this._generateMain();
 
 
-    // Java
-    this.template(javaDirTemplate + 'Application.java', javaDir + 'Application.java', this, {});
-    this.template(javaDirTemplate + 'config/ApplicationSettings.java', javaDir + 'config/ApplicationSettings.java', this, {});
-    this.template(javaDirTemplate + 'config/SecurityConfig.java', javaDir + 'config/SecurityConfig.java', this, {});
-    this.template(javaDirTemplate + 'config/CustomPermissionEvaluator.java', javaDir + 'config/CustomPermissionEvaluator.java', this, {});
-    this.template(javaDirTemplate + 'rest/controller/HomeController.java', javaDir + 'rest/controller/HomeController.java', this, {});
-    this.template(javaDirTemplate + 'rest/assembler/package-info.java', javaDir + 'rest/assembler/package-info.java', this, {});
-    //this.template(javaDirTemplate + 'rest/global/GlobalExceptionHandler.java', javaDir + 'rest/global/GlobalExceptionHandler.java', this, {});
-    this.template(javaDirTemplate + 'core/package-info.java', javaDir + 'core/package-info.java', this, {});
+    }
 
+    install() {
+        this.config.set('packageName', this.packageName);
+        this.config.set('packageFolder', this.packageFolder);
+    }
 
-    // Project
-    this.template(restDirTemplate + 'pom.xml', restDir + 'pom.xml', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
+    end() {
 
-    // ----------------------------
-    // Micro service starter MODEL
-    // ----------------------------
-    var modelDir = this.baseName + '-model/';
-    var modelDirTemplate = 'microservice-starter-model/';
-    var modelJavaDir = modelDir + 'src/main/java/' + packageFolder + '/model/';
-    var modelJavaDirTemplate = modelDirTemplate + 'src/main/java/package/';
-    var modelResourceDir = modelDir + 'src/main/resources/';
-    var modelResourceDirTemplate = modelDirTemplate + 'src/main/resources/';
-    var modelTestDir = modelDir + 'src/test/java/' + packageFolder + '/model/';
-    var modelTestDirTemplate = modelDirTemplate + 'src/test/java/package/';
+    }
 
+    _generateRest() {
+        const restDir = this.baseName + '-rest/';
+        const restDirTemplate = 'microservice-starter-rest/';
+        const javaDir = restDir + 'src/main/java/' + this.packageFolder + '/';
+        const javaDirTemplate = restDirTemplate + 'src/main/java/package/';
+        const resourceDir = restDir + 'src/main/resources/';
+        const resourceDirTemplate = restDirTemplate + 'src/main/resources/';
+        const testDir = restDir + 'src/test/java/' + this.packageFolder + '/';
+        const testDirTemplate = restDirTemplate + 'src/test/java/package/';
 
-    // Java
-    this.template(modelJavaDirTemplate + 'package-info.java', modelJavaDir + 'package-info.java', this, {});
+        // Resources
+        this.fs.copyTpl(
+            this.templatePath(resourceDirTemplate + 'application.yml'),
+            this.destinationPath(resourceDir + 'application.yml'),
+            {
+                baseName: this.baseName,
+                serviceDescription: this.serviceDescription
+            }
+        );
 
-    // Project
-    this.template(modelDirTemplate + 'pom.xml', modelDir + 'pom.xml', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
+        this.fs.copyTpl(
+            this.templatePath(resourceDirTemplate + 'bootstrap.yml'),
+            this.destinationPath(resourceDir + 'bootstrap.yml'),
+            {
+                baseName: this.baseName
+            }
+        );
 
-    // ----------------------------
-    // Micro service starter IT
-    // ----------------------------
-    var itDir = this.baseName + '-it/';
-    var itDirTemplate = 'microservice-starter-it/';
-    var itResourceDir = itDir + 'src/test/resources/';
-    var itResourceDirTemplate = itDirTemplate + 'src/test/resources/';
-    var itTestDir = itDir + 'src/test/java/' + packageFolder + '/it/';
-    var itTestDirTemplate = itDirTemplate + 'src/test/java/package/';
+        // Test
+        this.fs.copyTpl(
+            this.templatePath(testDirTemplate + 'rest/controller/HomeControllerTest.java'),
+            this.destinationPath(testDir + 'rest/controller/HomeControllerTest.java'),
+            {
+                packageName: this.packageName
 
-    // Java
-    this.template(itTestDirTemplate + 'IntegrationTest.java', itTestDir + 'IntegrationTest.java', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
+            }
+        );
 
-    // Project
-    this.template(itDirTemplate + 'pom.xml', itDir + 'pom.xml', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
+        this.fs.copyTpl(
+            this.templatePath(testDirTemplate + 'core/package-info.java'),
+            this.destinationPath(testDir + 'core/package-info.java'),
+            {
+                packageName: this.packageName
+            }
+        );
 
-    // Resource
-    this.template(itResourceDirTemplate + 'mock1.json', itResourceDir + 'mock1.json', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
+        // Java
+        this.fs.copyTpl(
+            this.templatePath(javaDirTemplate + 'Application.java'),
+            this.destinationPath(javaDir + 'Application.java'),
+            {
+                packageName: this.packageName
+            }
+        );
 
+        this.fs.copyTpl(
+            this.templatePath(javaDirTemplate + 'config/ApplicationSettings.java'),
+            this.destinationPath(javaDir + 'config/ApplicationSettings.java'),
+            {
+                packageName: this.packageName
+            }
+        );
 
-    // ----------------------------
-    // Micro service starter MAIN
-    // ----------------------------
-    this.template('pom.xml', 'pom.xml', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
-    this.template('.npmignore', '.gitignore', this, {});
+        this.fs.copyTpl(
+            this.templatePath(javaDirTemplate + 'config/SecurityConfig.java'),
+            this.destinationPath(javaDir + 'config/SecurityConfig.java'),
+            {
+                packageName: this.packageName
+            }
+        );
 
-    this.config.set('packageName', this.packageName);
-    this.config.set('packageFolder', packageFolder);
-};
+        this.fs.copyTpl(
+            this.templatePath(javaDirTemplate + 'config/CustomPermissionEvaluator.java'),
+            this.destinationPath(javaDir + 'config/CustomPermissionEvaluator.java'),
+            {
+                packageName: this.packageName
+            }
+        );
 
-SpringGenerator.prototype.projectfiles = function projectfiles() {
+        this.fs.copyTpl(
+            this.templatePath(javaDirTemplate + 'rest/controller/HomeController.java'),
+            this.destinationPath(javaDir + 'rest/controller/HomeController.java'),
+            {
+                packageName: this.packageName
+            }
+        );
 
+        this.fs.copyTpl(
+            this.templatePath(javaDirTemplate + 'core/package-info.java'),
+            this.destinationPath(javaDir + 'core/package-info.java'),
+            {
+                packageName: this.packageName
+            }
+        );
+
+        // Project
+        this.fs.copyTpl(
+            this.templatePath(restDirTemplate + 'pom.xml'),
+            this.destinationPath(restDir + 'pom.xml'),
+            {
+                baseName: this.baseName,
+                dockerRegistry: this.dockerRegistry,
+                dockerPrefix: this.dockerPrefix,
+                packageName: this.packageName
+            }
+        );
+    }
+
+    _generateModel() {
+        const modelDir = this.baseName + '-model/';
+        const modelDirTemplate = 'microservice-starter-model/';
+        const modelJavaDir = modelDir + 'src/main/java/' + this.packageFolder + '/model/';
+        const modelJavaDirTemplate = modelDirTemplate + 'src/main/java/package/';
+
+        // Java
+        this.fs.copyTpl(
+            this.templatePath(modelJavaDirTemplate + 'package-info.java'),
+            this.destinationPath(modelJavaDir + 'package-info.java'),
+            {
+                packageName: this.packageName
+            }
+        );
+
+        // Project
+        this.fs.copyTpl(
+            this.templatePath(modelDirTemplate + 'pom.xml'),
+            this.destinationPath(modelDir + 'pom.xml'),
+            {
+                baseName: this.baseName,
+                dockerRegistry: this.dockerRegistry,
+                dockerPrefix: this.dockerPrefix,
+                packageName: this.packageName
+            }
+        );
+
+    }
+
+    _generateIT() {
+        const itDir = this.baseName + '-it/';
+        const itDirTemplate = 'microservice-starter-it/';
+        const itResourceDir = itDir + 'src/test/resources/';
+        const itResourceDirTemplate = itDirTemplate + 'src/test/resources/';
+        const itTestDir = itDir + 'src/test/java/' + this.packageFolder + '/it/';
+        const itTestDirTemplate = itDirTemplate + 'src/test/java/package/';
+
+        // Java
+        this.fs.copyTpl(
+            this.templatePath(itTestDirTemplate + 'IntegrationTest.java'),
+            this.destinationPath(itTestDir + 'IntegrationTest.java'),
+            {
+                packageName: this.packageName
+            }
+        );
+
+        // Project
+        this.fs.copyTpl(
+            this.templatePath(itDirTemplate + 'pom.xml'),
+            this.destinationPath(itDir + 'pom.xml'),
+            {
+                baseName: this.baseName,
+                dockerRegistry: this.dockerRegistry,
+                dockerPrefix: this.dockerPrefix,
+                packageName: this.packageName
+            }
+        );
+
+        // Resources
+        this.fs.copy(
+            this.templatePath(itResourceDirTemplate + 'mock1.json'),
+            this.destinationPath(itResourceDir + 'mock1.json')
+        )
+    }
+
+    _generateMain() {
+        // Project
+        this.fs.copyTpl(
+            this.templatePath('pom.xml'),
+            this.destinationPath('pom.xml'),
+            {
+                baseName: this.baseName,
+                dockerRegistry: this.dockerRegistry,
+                dockerPrefix: this.dockerPrefix,
+                packageName: this.packageName,
+                serviceDescription: this.serviceDescription
+            }
+        );
+
+        this.fs.copyTpl(
+            this.templatePath('.npmignore'),
+            this.destinationPath('.npmignore'),
+            {
+                baseName: this.baseName
+            }
+        );
+    }
 };
